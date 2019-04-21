7345a26c1274915452499c1cee9a266e7932f1fd
JavierO <javier.ortiz13@upr.edu>
2019-04-21 19:34:26 -0400

    fixed http method (now everything is a get method

--- src/functionality/server.py
+++ src/functionality/server.py
@@ -30,33 +30,26 @@ class Server():
     def add_route(self, route_id, server_id, route):
         self.variables[route_id] = Route_ID(route, server_id)
 
-    def add_endpoints(self, server_id, route_endpoint, action, methods = None):
+    def add_endpoints(self, server_id, route_endpoint, action):
         app = self.variables[server_id].flask_instance
 
-        #idea: make dict with routes/actions and run that
-        #todo call all relevant functions base on given routes
         # app.add_url_rule('/', 'Home', lambda : 'Hello, World')
-        if(methods not None):
-            app.add_url_rule(route_endpoint, str(route_endpoint), action, methods)
-        else:
-            app.add_url_rule(route_endpoint, str(route_endpoint), action)
+
+        app.add_url_rule(route_endpoint, str(route_endpoint), action)
 
         return
 
-    def create_data(self, route_id, object_id):#PUT and POST
-        ## TODO:
+    def create_data(self, route_id, object_id):#TODO PUT and POST
         object = self.variables[object_id]
         endpoint = self.variables[route_id]
 
-        ## TODO: create action and call add_endpoints
-        def create_action(obj):
-            obj = request.json
+        def create_action():
+            object = request.get_json()
+            return str(object)
 
-        self.add_endpoints(endpoint.server_id, endpoint.route, create_action(object), methods = ['POST', 'PUT'])
+        self.add_endpoints(endpoint.server_id, endpoint.route, create_action)
 
-        pass
-
-    def read_data(self, route_id, object_id):#GET
+    def read_data(self, route_id, object_id):#TODO GET
         ## TODO:
         pass
 
--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     s.create_server("server", 5000)
     s.update_variables("data", '{}')
     s.update_variables("data2", '{"name": "Juana", "lastName": "Petraca"}')
-    s.add_route("send", "server", "/app/send")
+    s.add_route("send", "server", '/app/send')
+    s.create_data("send", "data")
     s.add_endpoints("server", '/', lambda : 'Hello, World')#placeholder, necessary for execution
     s.start_server("server")
