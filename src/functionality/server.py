0c979406bcf358be20cdbbab801b91b1dbf593f2
JavierO <javier.ortiz13@upr.edu>
2019-04-21 18:52:05 -0400

    added more functionality

--- src/functionality/server.py
+++ src/functionality/server.py
@@ -1,26 +1,20 @@
 from flask import Flask
+from flask import request
 import json
-
-# app = Flask(__name__)
-class EndpointAction(object):
-
-    def __init__(self, action):
-        self.action = action
-        self.response = Response(status=200, headers={})
-
-    def __call__(self, *args):
-        self.action()
-        return self.response
 
 class Route_ID():
     def __init__(self, route, server_id):
         self.route = route
         self.server_id = server_id
 
+class Server_ID():
+    def __init__(self, flask_instance, port):
+        self.flask_instance = flask_instance
+        self.port = port
+
 class Server():
 
-    def __init__(self, variables={}, port=80):
-        self.app_port = port
+    def __init__(self, variables={}):
         self.variables = variables #parser stores id's to this dict
 
     def update_variables(self, var_id, object):
@@ -37,7 +31,7 @@ class Server():
         self.variables[route_id] = Route_ID(route, server_id)
 
     def add_endpoints(self, server_id, route_endpoint, action):
-        app = self.variables[server_id]
+        app = self.variables[server_id].flask_instance
 
         #idea: make dict with routes/actions and run that
         #todo call all relevant functions base on given routes
@@ -52,6 +46,10 @@ class Server():
         endpoint = self.variables[route_id]
 
         ## TODO: create action and call add_endpoints
+        def create_action(object, new_object):
+            object = new_object
+
+        self.add_endpoints(endpoint.server_id, endpoint.route, )
 
         pass
 
@@ -60,8 +58,7 @@ class Server():
         pass
 
     def create_server(self, assigned_id, port=80):
-        self.variables[assigned_id] = Flask(assigned_id)
-        self.app_port = port
+        self.variables[assigned_id] = Server_ID(Flask(assigned_id), port)
 
     def start_server(self, server_id):
-        self.variables[server_id].run(port=self.app_port)
+        self.variables[server_id].flask_instance.run(port=self.variables[server_id].port)
