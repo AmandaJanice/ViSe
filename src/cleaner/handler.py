1287deba64979264d048f5320f2b9c8ecac750ab
JavierO <javier.ortiz13@upr.edu>
2019-04-23 16:27:56 -0400

    implemented handler methods

--- src/cleaner/handler.py
+++ src/cleaner/handler.py
@@ -1,11 +1,23 @@
 #returns message if not defined when called
 def id_not_defined(id):
-    pass
+    return "ID, " + str(id) + ", is not defined"
 
 #returns message id created when called
 def id_saved(id):
-    pass
+    return str(id) + " declared succesfully"
 
 #returns original string without the quotes
 def string_cleaner(line):
-    pass
+    if(isinstance(line, str)):
+        return line[1:-1]
+    return None
+
+
+# Testing
+
+# print(id_not_defined("some_id"))
+# print(id_saved("another_id"))
+# text = "\"text with brackets\""
+# print(text)
+# print(string_cleaner(text))
+# print(string_cleaner(234))
